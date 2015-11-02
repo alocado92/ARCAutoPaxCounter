3e65de4e09f04862f498c9239321779ebd13e977
alocado92 <alexis.figueroa4@upr.edu>
2015-11-02 12:38:20 -0400

    revert

--- web_app/server.js
+++ web_app/server.js
@@ -69,7 +69,7 @@ app.post('/login', function(req, res){
 	var password = req.body.pword;
 	
 	console.log('User: '+username +'\n'+'PW: '+password);
-	res.send({redirect: 'arcinnovations.ece.uprm.edu:3000/home'});
+	res.send({redirect: '/home'});
 	
 });
 app.get('/', function(req,res){
