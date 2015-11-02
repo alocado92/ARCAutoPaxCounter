e6f2fec60bc4a4da77c60138f66aa6faa46a0fd9
alocado92 <alexis.figueroa4@upr.edu>
2015-11-02 11:09:05 -0400

    cambio pa cristina

--- web_app/server.js
+++ web_app/server.js
@@ -76,6 +76,10 @@ app.get('/', function(req,res){
 	
 	res.sendFile("public/login.html", {"root": __dirname});
 });
+app.get('/mobile',function(req,res){
+	var v = req.body[0].entry_date;
+	res.send('Me llego un entry date' + v);
+});
 app.get('/home', function(req,res){
 	
 	res.send('<h1>Welcome Home </h1>');
