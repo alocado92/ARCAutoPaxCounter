22b29ac4f6a6549bac76462d4e8fa8e969aa2b97
alocado92 <alexis.figueroa4@upr.edu>
2015-11-02 09:54:52 -0400

    Initial commit web app

---
+++ web_app/public/login.js
@@ -0,0 +1,36 @@
+ (function() {
+	 var app = angular.module('app');
+	 
+    // Controller function and passing $http service and $scope var.
+    	app.controller('postController',['$http',function($http) {
+
+    		var vm = this;
+
+    		vm.hueletranca = "badilloxxx"
+
+      // create a blank object to handle form data.
+      	var data = {
+                uName: vm.username,
+                pword: vm.password
+            };
+        vm.user = {};
+
+      // calling our submit function.
+        vm.submitForm = function() {
+        		var data = {
+                uName: vm.username,
+                pword: vm.password
+            };
+        // Posting data to node.js server
+        $http.post("http://localhost:3000/login", data, {headers: {'Content-Type': 'application/json'} })
+        .then(function (response) {
+        	
+            window.location = response.data.redirect;
+            console.log('Win');
+        	console.log(response.data.redirect);
+            return response;
+        });
+        
+        };
+    }]);
+    })();
---
+++ web_app/server.js
@@ -0,0 +1,88 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var bodyParser = require('body-parser');
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : '< MySQL username >',
+  password : '< MySQL password >',
+  database : '<your database name>'
+});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ 
+   extended: true 
+}));
+var allowCrossDomain = function(req, res, next) {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+	res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+	// intercept OPTIONS method
+	if ('OPTIONS' == req.method) {
+		res.sendStatus(200);
+	} else {
+		next();
+	}
+};
+app.use(allowCrossDomain);
+app.use(express.static(path.join('./public')));
+
+
+app.get('/user/:user_id', function (req, res) {
+  res.send('Hello World!');
+  console.log('Hello ' + req.params.user_id);
+});
+app.post('/graph1', function (req, res){
+	var type = req.body.type;
+	var date_begin = req.body.date1;
+	var date_end = req.body.date2;
+	var route = req.body.route;
+	var graph_type ='';
+
+	if(type == 'Origin-Destination Matrix'){
+		graph_type = '';
+	}
+	else if (type == 'Net Passenger Flow'){
+		graph_type = '';
+	}
+	else {
+		graph_type = '';
+	}
+
+	var query = '';
+	connection.connect();
+	connection.query(query, function(err,rows,field){
+		if(!err){
+			//do black magic
+		}
+		else{
+			//handle error like a boss
+		}
+		connection.end();
+	});
+});
+app.post('/login', function(req, res){
+	
+	console.log('wassap');
+	var username = req.body.uName;
+	var password = req.body.pword;
+	
+	console.log('User: '+username +'\n'+'PW: '+password);
+	res.send({redirect: '/home'});
+	
+});
+app.get('/', function(req,res){
+	
+	res.sendFile("public/login.html", {"root": __dirname});
+});
+app.get('/home', function(req,res){
+	
+	res.send('<h1>Welcome Home </h1>');
+});
+
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('Example app listening at http://%s:%s', host, port);
+});
