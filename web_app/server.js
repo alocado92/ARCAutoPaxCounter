19af82a72d550f719eacd7cec93ff83264838fc5
alocado92 <alexis.figueroa4@upr.edu>
2015-11-03 01:15:20 -0400

    ddd

--- web_app/server.js
+++ web_app/server.js
@@ -81,8 +81,8 @@ app.get('/', function(req,res){
 });
 app.post('/mobile', function(req,res){
 	//res.sendStatus(200);
-	var entry = req;
-	console.log(entry);
+	
+	console.log(req);
 	// if(entry[0].entry_lat >18.0){
 	// res.end('OK');}
 });
