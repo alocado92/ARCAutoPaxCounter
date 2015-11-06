1f60c0311e0d497fbdae6a4f991a60410938c528
alocado92 <alexis.figueroa4@upr.edu>
2015-11-05 20:55:21 -0400

    credential change db

--- web_app/mysqlpool.js
+++ web_app/mysqlpool.js
@@ -3,7 +3,7 @@ var pool  = mysql.createPool({
 	connectionLimit : 100,
     host     : 'localhost',
     user     : 'root',
-    password : 'icaip70o',
+    password : 'Kie2iedu',
     database : 'capstone'
 });
 
