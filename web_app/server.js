9e80413ccd6ffa507d76f669ef891798b0483667
alocado92 <alexis.figueroa4@upr.edu>
2015-11-10 17:38:30 -0400

    test

--- web_app/server.js
+++ web_app/server.js
@@ -88,7 +88,7 @@ app.post('/login', function (req, res){
 	log.info({User: username,Pass: hashed},'successful login detected!');
 	pool.getConnection(function(err, connection) {
 	  		// Use the connection
-	  		connection.query( 'select count(*) as check from User where username ="'+username+'" AND password ="'+hashed+'"', function (err, rows) {
+	  		connection.query( 'select count(*) AS check from User where username ="'+username+'" AND password ="'+hashed+'"', function (err, rows) {
 	   			//manipulate rows
 	   			console.log(rows);
 	   			connection.release();
