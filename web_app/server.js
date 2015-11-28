79427bd71c2bc5dd66770141ca6fb283abd0de14
alocado92 <alexis.figueroa4@upr.edu>
2015-11-28 17:51:44 -0400

    test

--- web_app/server.js
+++ web_app/server.js
@@ -428,9 +428,10 @@ app.post('/mobile', function (req,res){
 					
 					//passengers.push(req.body[i]);
 					pool.getConnection(function (err,connection){
-							connection.query('select stop_ID from Stop natural join Linked_to natural join Belongs inner join Trip
- 								where end_time is null', function (err, rows){
-								connection.query('select name, stop_latitude,stop_longitude from Stop where stop_ID in ?',rows, function (err, rows){
+							/*connection.query('select stop_ID from Stop natural join Linked_to natural join Belongs inner join Trip
+ 								where end_time is null', function (err, rows){*/
+								connection.query('select name, stop_latitude,stop_longitude from Stop where stop_ID in select stop_ID from Stop natural join Linked_to natural join Belongs inner join Trip
+ 								where end_time is null',rows, function (err, rows){
 									if(rows.length < 1){
 										console.log('There are no active trips. Please add an active trip in order to register passengers');
 									}
@@ -471,7 +472,7 @@ app.post('/mobile', function (req,res){
 
 									}
 								});
-							});
+							/*});*/
 					    	
 						});
 					
