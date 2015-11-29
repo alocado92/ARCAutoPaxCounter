05f2f6259c020d01e146a4e987bce90a62c23d86
alocado92 <alexis.figueroa4@upr.edu>
2015-11-28 20:23:16 -0400

    test

--- web_app/server.js
+++ web_app/server.js
@@ -429,7 +429,7 @@ app.post('/mobile', function (req,res){
 					//passengers.push(req.body[i]);
 					pool.getConnection(function (err, connection){
 							connection.query('Select stop_ID from Stop natural join Linked_to natural join Route natural join Belongs inner join Trip where end_time is null', function (err, rows){
-								console.log("Req.body.length: "+ test.body[0]);
+								console.log("Req.body.length: "+ test.body[0].entry_lat);
 								console.log("Rows after initial query "+rows);
 								var ling_ling = '';
 								for(var i=0; i<rows.length;i++){
