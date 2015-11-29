fd135e07ed359750b3e98e56c5b5c739cc77b83a
alocado92 <alexis.figueroa4@upr.edu>
2015-11-28 20:51:05 -0400

    test

--- web_app/server.js
+++ web_app/server.js
@@ -453,7 +453,7 @@ app.post('/mobile', function (req,res){
 									else{
 										console.log("test.body.log: "+ test.body[0].entry_lat);
 										for(var j=0; j<rows.length;j++){
-											console.log("test.body.log: "+ test.body[j].entry_log);
+											console.log("test.body.log: "+ test.body[j].entry_lat);
 											var dist = geolib.getDistance(
 											    {latitude: test.body[j].entry_lat, longitude: test.body[j].entry_log},
 											    {latitude: rows[j].stop_latitude, longitude: rows[j].stop_longitude}
