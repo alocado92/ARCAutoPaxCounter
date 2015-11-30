94c133cf3ec538946ed14e4f29cbd5220d33e65b
alocado92 <alexis.figueroa4@upr.edu>
2015-11-30 02:36:49 -0400

    test

--- web_app/server.js
+++ web_app/server.js
@@ -106,7 +106,7 @@ app.post('/graph1', function (req, res){
 					//result.push(fila);
 				}
 				//console.log('Initialized result: '+result);
-				connection.query('select passenger_ID from Passenger NATURAL JOIN Takes NATURAL JOIN Trip where start_time >= "'+date_begin+'" AND end_time <= "'+date_end+'"' ,function (err,rows){
+				connection.query('select passenger_ID from Passenger NATURAL JOIN Takes NATURAL JOIN Trip NATURAL JOIN Belongs NATURAL JOIN Route where start_time >= "'+date_begin+'" AND end_time <= "'+date_end+'" AND route_name= "' +route1+'"',function (err,rows){
 						var pass_id = '';
 						console.log('Rows length: '+rows.length);
 						for (var i=0; i< rows.length; i++){
@@ -134,7 +134,7 @@ app.post('/graph1', function (req, res){
 							console.log('Parada_id: '+parada_id);
 							console.log('Parada_id1: '+parada_id1);
 							console.log('Pass_id: '+pass_id);
-							connection.query('SELECT COUNT(origin_stop) as "Origin", origin_stop, dest_stop FROM Passenger WHERE (('+pass_id+') AND ('+parada_id+') AND ('+parada_id1+')) Group By (dest_stop) ORDER By (origin_stop)', function (err, rows){
+							connection.query('SELECT COUNT(origin_stop) as "Origin", origin_stop, dest_stop FROM Passenger WHERE (('+pass_id+') ) Group By (dest_stop) ORDER By (origin_stop)', function (err, rows){
 								/*for(var a=0;a<stops_name.length;a++){
 									for(var b=0; b<stops_name.length;b++){
 										if(stops_name[a] == rows[b].origin_stop){
