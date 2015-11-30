64e9dc7ed7dcdca070d53fc800630aff4d5e3d3d
alocado92 <alexis.figueroa4@upr.edu>
2015-11-30 03:07:07 -0400

    test

--- web_app/public/home.js
+++ web_app/public/home.js
@@ -120,33 +120,28 @@
         	var res = response.data;
         	var table = new Array();
             var data2 = new google.visualization.DataTable();
-                      // Add columns
-                      data2.addColumn('string', 'Origin/Destination');
-                      data2.addColumn('number', 'Fisica');
-                      data2.addColumn('number', 'PatioCentral');
-                      data2.addColumn('number', 'Biblioteca');
-                      data2.addColumn('number', 'CentroEstudiante');
-                      data2.addColumn('number', 'EdificioTPinero');
-                      data2.addColumn('number', 'TownCenter');
-                      data2.addColumn('number', 'GimnasioEspada');
-                      data2.addColumn('number', 'CITAI');
-                      data2.addColumn('number', 'EdificioA');
-                      data2.addColumn('number', 'Empresas');
-                      data2.addColumn('number', 'Vita');
-
-                      data2.addRow(['Fisica',0,2,3,4,5,6,7,8,9,12,13]);
-                        data2.addRow(['PatioCentral',4,0,3,6,4,5,4,6,422,34,55]);
-                        data2.addRow(['Biblioteca',11,22,0,44,55,66,77,88,5,3,5]);
-                        data2.addRow(['CentroEstudiante',21,11,22,0,4,5,6,7,8,9,7]);
-                        data2.addRow(['EdificioTPinero',23,11,12,13,0,15,16,5,6,5,5]);
-                        data2.addRow(['TownCenter',1,2,3,4,5,0,7,8,9,10,11]);
-                        data2.addRow(['GimnasioEspada',10,9,8,7,6,5,0,3,2,1,22]);
-                        data2.addRow(['CITAI',1,2,3,4,5,6,7,0,8,9,11]);
-                        data2.addRow(['EdificioA',2,3,4,5,6,7,8,9,0,12,11]);
-                        data2.addRow(['Empresas',1,2,3,4,5,6,7,8,9,0,13]);
-                        data2.addRow(['Vita',2,3,4,5,6,7,8,9,10,11,0]);
+                     
             if (vm.gtype2 == '1'){
-                
+                      data2.addColumn('string', 'Origin/Destination');
+                      for(var d=0;d<stop.length;d++){
+                        data2.addColumn('number', stop[d].toString());
+                      }
+                      
+                      
+                      for(var e=0;e<stop.length;e++){
+                        var row = [stop[e].toString()];
+                        for(var f=0;f<stop.length;f++){
+                          if(stop[e] == res[f].origin_stop){
+                            if(stop[f]==res[f].dest_stop){
+                              row.push(res[f].Origin);
+                            }
+                            else{
+                              row.push(0);
+                            }
+                          }
+                        }
+                        data2.addRow(row);
+                      }
                       var table2 = new google.visualization.Table(document.getElementById('chart2'));
                       table2.draw(data2, {showRowNumber: true, width: '100%', height: '100%'});
             }
