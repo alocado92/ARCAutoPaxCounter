68fffc0e2cc7c86ca811036ee5811e9dfb3be28e
alocado92 <alexis.figueroa4@upr.edu>
2015-11-30 02:59:55 -0400

    test

--- web_app/public/home.js
+++ web_app/public/home.js
@@ -47,7 +47,7 @@
                       for(var e=0;e<stop.length;e++){
                         var row = [stop[e].toString()];
                         for(var f=0;f<stop.length;f++){
-                          if(stop[e] == res[e].origin_stop){
+                          if(stop[e] == res[f].origin_stop){
                             if(stop[f]==res[f].dest_stop){
                               row.push(res[f].Origin);
                             }
