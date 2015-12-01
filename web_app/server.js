fac4ecf315f4a0961bbeb4e99e52f538b568a322
alocado92 <alexis.figueroa4@upr.edu>
2015-12-01 14:14:02 -0400

    test

--- web_app/server.js
+++ web_app/server.js
@@ -44,7 +44,7 @@ var allowCrossDomain = function(req, res, next) {
 	res.header('Access-Control-Allow-Origin', '*');
 	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
 	res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-	res.setHeader('Content-disposition', 'attachment; filename=data.json');
+	//res.setHeader('Content-disposition', 'attachment; filename=data.json');
 
 	// intercept OPTIONS method
 	if ('OPTIONS' == req.method) {
