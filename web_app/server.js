409dbde43a9ae1f6b60835bbdfecde18b7485cd6
alocado92 <alexis.figueroa4@upr.edu>
2015-12-02 23:36:59 -0400

    test

--- web_app/server.js
+++ web_app/server.js
@@ -1080,9 +1080,9 @@ app.post('/mobile', function (req,res){
 								    		var pass_id = rows.insertId;
 								    		//console.log("i = "+il);
 								    		console.log("successfully inserted passenger with id: "+rows.insertId);
-								    		connection.query('Select trip_ID from Trip where end_time is null AND name ="'+study_name+'"', function (err,rows){
-								    			console.log('Trip ID: '+ rows[0].trip_ID);
-								    			console.log('Passenger ID: '+ pass_id);
+								    		connection.query('Select trip_ID from Trip where end_time is null AND name ='+study_name, function (err,rows){
+								    			//console.log('Trip ID: '+ rows[0].trip_ID);
+								    			//console.log('Passenger ID: '+ pass_id);
 								    			var relation = {passenger_ID: pass_id, trip_ID: rows[0].trip_ID};
 								    			connection.query('Insert into Takes SET ?', relation , function (err, rows){
 								    				/*count++;
