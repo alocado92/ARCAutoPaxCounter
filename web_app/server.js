95c06de5c1a83659b5aabc4be79217ac9f0e12f1
alocado92 <alexis.figueroa4@upr.edu>
2015-12-03 05:21:48 -0400

    test headers

--- web_app/server.js
+++ web_app/server.js
@@ -50,9 +50,9 @@ app.use(bodyParser.urlencoded({
    extended: true 
 }));
 var allowCrossDomain = function(req, res, next) {
-	res.header('Access-Control-Allow-Origin', '*');
-	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-	res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+	//res.header('Access-Control-Allow-Origin', '*');
+	//res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+	//res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
 	//res.setHeader('Content-disposition', 'attachment; filename=data.json');
 
 	// intercept OPTIONS method
