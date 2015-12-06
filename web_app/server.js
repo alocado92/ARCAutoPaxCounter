afc40e6d1af1c9337120e192236daab2a1050be5
alocado92 <alexis.figueroa4@upr.edu>
2015-12-06 12:39:00 -0400

    test

--- web_app/server.js
+++ web_app/server.js
@@ -834,7 +834,7 @@ app.post('/add', function (req,res){
 		});
 	});
 app.get('/', function (req,res){
-	req.session = null;
+	req.session.destroy()
 	res.sendFile("public/login.html", {"root": __dirname});
 });
 app.get('/addRoute', function (req,res){
